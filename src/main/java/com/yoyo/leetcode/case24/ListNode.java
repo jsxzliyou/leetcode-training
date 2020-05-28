014c4444a3bfed2dd4a30f23d266cbf32550cf19
yylic <yylic@isoftstone.com>
2020-05-29 00:16:46 +0800

    case

---
+++ src/main/java/com/yoyo/leetcode/case24/ListNode.java
@@ -0,0 +1,9 @@
+package main.java.com.yoyo.leetcode.case24;
+
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) {
+        val = x;
+    }
+}
---
+++ src/main/java/com/yoyo/leetcode/case24/SwapPairs.java
@@ -0,0 +1,36 @@
+package main.java.com.yoyo.leetcode.case24;
+
+import java.util.List;
+
+public class SwapPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        //定义返回结果
+        ListNode result = new ListNode(-1);
+        result.next = head;
+        //记录前继节点
+        ListNode pre = result;
+        while (head != null && head.next != null) {
+
+            ListNode left = head;
+            ListNode right = head.next;
+
+            pre.next = right;
+            left.next = right.next;
+            right.next = left;
+
+            pre = left;
+            head = left.next;
+        }
+        return result.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode ln = new ListNode(1);
+        ln.next = new ListNode(2);
+        ln.next.next = new ListNode(3);
+        ln.next.next.next = new ListNode(4);
+
+        ListNode result = new SwapPairs().swapPairs(ln);
+    }
+}
