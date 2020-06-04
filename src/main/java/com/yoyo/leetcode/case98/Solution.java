1f169e888aa9f81ee3047fed5aad76a27e449704
Atticus Lee <jsxzliyou@163.com>
2020-06-04 15:17:15 +0800

    判断是否二叉搜索树

---
+++ src/main/java/com/yoyo/leetcode/case98/Solution.java
@@ -0,0 +1,22 @@
+package main.java.com.yoyo.leetcode.case98;
+
+public class Solution {
+
+    public boolean isValidBST(TreeNode root) {
+
+        return isValid(root,null,null);
+    }
+
+    public boolean isValid(TreeNode root,Integer min,Integer max) {
+        if (root == null) return true;
+        if (min != null && root.val <= min) return false;
+        if (max != null && root.val >= max) return false;
+        return isValid(root.left,min,root.val) && isValid(root.right,root.val,max);
+    }
+
+    public static void main(String[] args) {
+        TreeNode rootN = new TreeNode(2);
+        rootN.left = new TreeNode(1);
+        rootN.right = new TreeNode(3);
+    }
+}
---
+++ src/main/java/com/yoyo/leetcode/case98/TreeNode.java
@@ -0,0 +1,12 @@
+package main.java.com.yoyo.leetcode.case98;
+
+public class TreeNode {
+
+    int val;
+
+    TreeNode left,right;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
